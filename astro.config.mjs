3600daa801f70edcfa8733bfe7a61cee7618aa7d
Christian Torrico <christian.torrico-contractor@procore.com>
2024-11-27 18:19:06 -0400

    added react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import aws from 'astro-sst'
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), react()],
   output: 'server',
   adapter: aws({
     deploymentStrategy: 'regional',
