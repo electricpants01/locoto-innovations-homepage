73ebedc2ee1d0be7f386efde447b76b67b4d5369
Christian Torrico <christian.torrico-contractor@procore.com>
2024-11-27 17:28:28 -0400

    added sst into the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
+import aws from 'astro-sst'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind(), icon()],
+  output: 'server',
+  adapter: aws({
+    deploymentStrategy: 'regional',
+    responseMode: 'stream',
+  })
 });
---
+++ sst.config.ts
@@ -0,0 +1,16 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "locoto-innovations-homepage",
+      removal: input?.stage === "prod" ? "retain" : "remove",
+      home: "aws",
+      region: 'us-east-1'
+    };
+  },
+  async run() {
+    new sst.aws.Astro("LocotoHomePage");
+
+  },
+});
