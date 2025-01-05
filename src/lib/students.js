8c1202d5d6c2ac1ba745521d716d6aa0d2e7e8e2
Christian Torrico <christian.torrico-contractor@procore.com>
2025-01-05 18:47:15 -0400

    updated SearchAndResults.astro

--- src/lib/students.js
+++ src/lib/students.js
@@ -1,10 +1,19 @@
 import { StudentsCollection } from "./mongodb";
 
-export const getAllStudents = async () => {
-    console.log("Getting all students");
+export const getAllStudents = async (searchQuery = "") => {
+    console.log("Getting students with search query:", searchQuery);
     const studentsCollection = await StudentsCollection();
-    const response = await studentsCollection.find({}).limit(5).toArray();
-    console.log("Fetched students:", response);
+
+    // If searchQuery is provided, filter students by name
+    const query = searchQuery ? {
+            $and: searchQuery.split(/\s+/).map(word => ({
+                fullName: { $regex: word, $options: "i" }
+            }))
+        } : {};
+    console.log(query);
+
+    const response = await studentsCollection.find(query).limit(30).toArray();
+    console.log("Fetched students:", response.length);
     return response;
 };
 
