37b6085fd054a6088251382ecda4e75258185b3e
christian torrico <christorricoavila@gmail.com>
2026-06-10 19:21:55 -0400

    added deployment github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://electricpants01.github.io',
+  base: '/locoto-innovations-homepage',
   vite: {
     plugins: [tailwindcss()]
   }
